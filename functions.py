000b3b1e4367176eb0db58b29e7976d99731f39e
zalapino <zalapino@msu.edu>
2019-04-16 18:29:10 -0400

    Revert "commit i guess"
    This reverts commit ef7938b9586d3e4926f8446babba0869a01e199c, reversing
    changes made to 79f16f218fa94b7b8f9fc9bca434808ab8ed0f90.

--- functions.py
+++
@@ -1,4 +0,0 @@
-w = widgets.Dropdown(               #creates a dropdown menu with the data keys from the dictionary above as options
-    options=mydata.keys(),
-    value = default,
-    description='Data',)
